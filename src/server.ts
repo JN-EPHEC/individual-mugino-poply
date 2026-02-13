f929b52a23b1afebc160b16f9a18d671739c7982
Hippolyte <hippolyte.amory@gmail.com>
2026-02-13 17:45:37 +0100

    ex 1

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,17 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Bienvenue sur mon serveur API');
 });
 
+app.get('/api/data', (req: Request, res: Response) => {
+    const etudiants = [
+        { id: 1, nom: "Dupont", prenom: "Jean" },
+        { id: 2, nom: "Martin", prenom: "Sophie" },
+        { id: 1, nom: "Doe", prenom: "John" },        
+    ];
+    res.json(etudiants);
+})
+
+
+
 app.listen(port, () => {
     console.log(`Serveur lancé sur http://localhost:${3000}`)
 });
