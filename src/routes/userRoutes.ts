0c68e9950701d7e8c181b4d2dbc45579136f8974
Hippolyte <hippolyte.amory@gmail.com>
2026-02-14 17:11:50 +0100

    Déplacement des routes dans userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,6 +2,32 @@ import express, {Router, Request, Response} from 'express';
 
 const router = Router();
 
+// Route racine
+router.get('/', (req: Request, res: Response) => {
+    res.send('Bienvenue sur mon serveur API');
+});
+
+// Route /api/data
+router.get('/api/data', (req: Request, res: Response) => {
+    const etudiants = [
+        { id: 1, nom: "Dupont", prenom: "Jean" },
+        { id: 2, nom: "Martin", prenom: "Sophie" },
+        { id: 1, nom: "Doe", prenom: "John" },        
+    ];
+    res.json(etudiants);
+});
+
+// Route /api/hello/nom_dynamique
+router.get('/api/hello/:name', (req: Request, res: Response) => {
+    const timestamp = new Date();
+    const nom = req.params.name;
+    const reponse = {
+        "message": `Bonjour ${nom}`,
+        "timestamp": timestamp
+    };
+    res.json(reponse);
+});
+
 // Route /api/users
 router.get('/api/users', (req: Request, res: Response) => {
     const users = [
