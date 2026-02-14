9496882113fb972ab3de73c96c464a81386a0878
Hippolyte <hippolyte.amory@gmail.com>
2026-02-14 17:08:05 +0100

    enfin réussi à lier la DB

---
+++ src/models/User.ts
@@ -0,0 +1,24 @@
+import { Sequelize, DataTypes, Model } from 'sequelize';
+import sequelize from '../config/database';
+
+
+class User extends Model {}
+
+User.init(
+    {
+        nom: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        prenom: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'User',
+    },
+);
+
+console.log(User === sequelize.models.User);
--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,56 @@
 import express, {Application, Request, Response} from 'express';
 import userRoutes from './routes/userRoutes';
+import sequelize from './config/database';
+import { initDatabase } from './config/database';
+import User from './models/User';
 
-const app: Application = express();
-const port = 3000;
+const startServer = async () => {
+    await initDatabase(); // On vérifie la connexion avec la DB
 
-app.use(express.json()); // important
+    await sequelize.sync({ alter: true });  // On synchronise les modèles
+    console.log('Tous les modèles synchronisés avec la base de données.');
 
-app.use('/', userRoutes); // on monte à la racine
+    const app: Application = express(); // Config express
 
 
-// Route racine
-app.get('/', (req: Request, res: Response) => {
-    res.send('Bienvenue sur mon serveur API');
-});
+    app.use(express.json()); // important
 
-// Route /api/data
-app.get('/api/data', (req: Request, res: Response) => {
-    const etudiants = [
-        { id: 1, nom: "Dupont", prenom: "Jean" },
-        { id: 2, nom: "Martin", prenom: "Sophie" },
-        { id: 1, nom: "Doe", prenom: "John" },        
-    ];
-    res.json(etudiants);
-});
+    app.use('/', userRoutes); // on monte à la racine
 
-// Route /api/hello/nom_dynamique
-app.get('/api/hello/:name', (req: Request, res: Response) => {
-    const timestamp = new Date();
-    const nom = req.params.name;
-    const reponse = {
-        "message": `Bonjour ${nom}`,
-        "timestamp": timestamp
-    };
-    res.json(reponse);
-});
+    // Route racine
+    app.get('/', (req: Request, res: Response) => {
+        res.send('Bienvenue sur mon serveur API');
+    });
 
-app.listen(port, () => {
-    console.log(`Serveur lancé sur http://localhost:${port}`)
+    // Route /api/data
+    app.get('/api/data', (req: Request, res: Response) => {
+        const etudiants = [
+            { id: 1, nom: "Dupont", prenom: "Jean" },
+            { id: 2, nom: "Martin", prenom: "Sophie" },
+            { id: 1, nom: "Doe", prenom: "John" },        
+        ];
+        res.json(etudiants);
+    });
+
+    // Route /api/hello/nom_dynamique
+    app.get('/api/hello/:name', (req: Request, res: Response) => {
+        const timestamp = new Date();
+        const nom = req.params.name;
+        const reponse = {
+            "message": `Bonjour ${nom}`,
+            "timestamp": timestamp
+        };
+        res.json(reponse);
+    });
+
+    const port = 3000; // Config port
+
+    app.listen(port, () => {
+        console.log(`Serveur lancé sur http://localhost:${port}`)
+    });
+};
+
+startServer().catch((err) => {
+  console.error('Erreur au démarrage du serveur :', err);
+  process.exit(1);
 });
